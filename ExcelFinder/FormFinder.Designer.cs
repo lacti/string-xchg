4bc8ce6fb24ae6a68cd12f91293b0f0ffdb27172
lacti <lactrious@gmail.com>
2014-10-29 02:36:48 +0900

    add excel-finder

---
+++ ExcelFinder/FormFinder.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ExcelFinder
+{
+    partial class FormFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listPaths = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textKeyword = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listResult = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.backgroundFinder = new System.ComponentModel.BackgroundWorker();
+            this.buttonRemoveFolder = new System.Windows.Forms.Button();
+            this.buttonFindFolder = new System.Windows.Forms.Button();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelStatus,
+            this.toolStripStatusLabel2,
+            this.progressBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 557);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(866, 25);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(40, 20);
+            this.labelStatus.Text = "Wait";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(709, 20);
+            this.toolStripStatusLabel2.Spring = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(100, 19);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Paths";
+            // 
+            // listPaths
+            // 
+            this.listPaths.AllowDrop = true;
+            this.listPaths.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listPaths.FormattingEnabled = true;
+            this.listPaths.ItemHeight = 15;
+            this.listPaths.Location = new System.Drawing.Point(15, 27);
+            this.listPaths.Name = "listPaths";
+            this.listPaths.Size = new System.Drawing.Size(758, 94);
+            this.listPaths.TabIndex = 1;
+            this.listPaths.DragDrop += new System.Windows.Forms.DragEventHandler(this.listPaths_DragDrop);
+            this.listPaths.DragEnter += new System.Windows.Forms.DragEventHandler(this.listPaths_DragEnter);
+            this.listPaths.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listPaths_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Keyword";
+            // 
+            // textKeyword
+            // 
+            this.textKeyword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textKeyword.Location = new System.Drawing.Point(15, 160);
+            this.textKeyword.Name = "textKeyword";
+            this.textKeyword.Size = new System.Drawing.Size(758, 25);
+            this.textKeyword.TabIndex = 5;
+            this.textKeyword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textKeyword_KeyDown);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSearch.Location = new System.Drawing.Point(779, 160);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 25);
+            this.buttonSearch.TabIndex = 6;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Results";
+            // 
+            // listResult
+            // 
+            this.listResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listResult.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader4,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listResult.FullRowSelect = true;
+            this.listResult.GridLines = true;
+            this.listResult.Location = new System.Drawing.Point(15, 231);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(839, 310);
+            this.listResult.TabIndex = 8;
+            this.listResult.UseCompatibleStateImageBehavior = false;
+            this.listResult.View = System.Windows.Forms.View.Details;
+            this.listResult.DoubleClick += new System.EventHandler(this.listResult_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File";
+            this.columnHeader1.Width = 381;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Address";
+            this.columnHeader2.Width = 72;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Text";
+            this.columnHeader3.Width = 376;
+            // 
+            // backgroundFinder
+            // 
+            this.backgroundFinder.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundFinder_DoWork);
+            this.backgroundFinder.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundFinder_ProgressChanged);
+            this.backgroundFinder.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundFinder_RunWorkerCompleted);
+            // 
+            // buttonRemoveFolder
+            // 
+            this.buttonRemoveFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRemoveFolder.Location = new System.Drawing.Point(779, 96);
+            this.buttonRemoveFolder.Name = "buttonRemoveFolder";
+            this.buttonRemoveFolder.Size = new System.Drawing.Size(75, 25);
+            this.buttonRemoveFolder.TabIndex = 3;
+            this.buttonRemoveFolder.Text = "Remove";
+            this.buttonRemoveFolder.UseVisualStyleBackColor = true;
+            this.buttonRemoveFolder.Click += new System.EventHandler(this.buttonRemoveFolder_Click);
+            // 
+            // buttonFindFolder
+            // 
+            this.buttonFindFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonFindFolder.Location = new System.Drawing.Point(779, 65);
+            this.buttonFindFolder.Name = "buttonFindFolder";
+            this.buttonFindFolder.Size = new System.Drawing.Size(75, 25);
+            this.buttonFindFolder.TabIndex = 2;
+            this.buttonFindFolder.Text = "Add";
+            this.buttonFindFolder.UseVisualStyleBackColor = true;
+            this.buttonFindFolder.Click += new System.EventHandler(this.buttonFindFolder_Click);
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Sheet";
+            this.columnHeader4.Width = 100;
+            // 
+            // FormFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 582);
+            this.Controls.Add(this.listResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonFindFolder);
+            this.Controls.Add(this.buttonRemoveFolder);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textKeyword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listPaths);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "FormFinder";
+            this.Text = "Excel Finder";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labelStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listPaths;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textKeyword;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listResult;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.ComponentModel.BackgroundWorker backgroundFinder;
+        private System.Windows.Forms.Button buttonRemoveFolder;
+        private System.Windows.Forms.Button buttonFindFolder;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ ExcelFinder/FormFinder.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace ExcelFinder
+{
+    public partial class FormFinder : Form
+    {
+        public FormFinder()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFindFolder_Click(object sender, EventArgs e)
+        {
+            var chooser = new FolderBrowserDialog {Description = "Select a path for searching"};
+            if (chooser.ShowDialog(this) == DialogResult.OK)
+            {
+                listPaths.Items.Add(chooser.SelectedPath);
+            }
+        }
+
+        private void buttonRemoveFolder_Click(object sender, EventArgs e)
+        {
+            if (listPaths.SelectedIndex >= 0)
+            {
+                listPaths.Items.RemoveAt(listPaths.SelectedIndex);
+            }
+        }
+
+        private void listPaths_DragDrop(object sender, DragEventArgs e)
+        {
+            var paths = (string[]) e.Data.GetData(DataFormats.FileDrop);
+            foreach (var path in paths)
+            {
+                if (Directory.Exists(path) || File.Exists(path))
+                {
+                    listPaths.Items.Add(path);
+                }
+            }
+        }
+
+        private void listPaths_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void listPaths_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                buttonRemoveFolder_Click(sender, e);
+            }
+        }
+
+        private void textKeyword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buttonSearch_Click(sender, e);
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (backgroundFinder.IsBusy)
+                return;
+
+            buttonSearch.Enabled = false;
+            textKeyword.Enabled = false;
+            listResult.Items.Clear();
+
+            backgroundFinder.RunWorkerAsync();
+        }
+
+        private void backgroundFinder_DoWork(object sender, DoWorkEventArgs e)
+        {
+            UpdateStatus("Preparing files...");
+
+            var excelFiles = new List<string>();
+            foreach (string path in listPaths.Items)
+            {
+                if (File.Exists(path)) excelFiles.Add(path);
+                else if (Directory.Exists(path))
+                {
+                    excelFiles.AddRange(FindFiles(path, "*.xls"));
+                    excelFiles.AddRange(FindFiles(path, "*.xlsx"));
+                }
+            }
+            excelFiles = excelFiles.Distinct().ToList();
+            excelFiles.Sort();
+
+            var keyword = textKeyword.Text.Trim();
+            if (string.IsNullOrWhiteSpace(keyword))
+                return;
+
+            foreach (var excelFile in excelFiles)
+            {
+                UpdateStatus("Search at " + Path.GetFileName(excelFile));
+                try
+                {
+                    Search(excelFile, keyword);
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        private void Search(string excelFile, string keyword)
+        {
+            using (var workbook = new XLWorkbook(excelFile))
+            {
+                foreach (var worksheet in workbook.Worksheets)
+                {
+                    var range = worksheet.RangeUsed();
+                    if (range == null)
+                        continue;
+
+                    for (var row = 1; row <= range.RowCount(); ++row)
+                    {
+                        for (var column = 1; column <= range.ColumnCount(); ++column)
+                        {
+                            var cell = range.Cell(row, column);
+                            if (cell == null)
+                                continue;
+
+                            try
+                            {
+                                var value = cell.GetValue<string>();
+                                if (string.IsNullOrWhiteSpace(value))
+                                    continue;
+
+                                if (value.Contains(keyword))
+                                {
+                                    AddResult(excelFile, worksheet.Name, cell.Address.ToString(), value);
+                                }
+                            }
+                            catch
+                            {
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UpdateStatus(string message)
+        {
+            if (InvokeRequired) Invoke(new MethodInvoker(() => UpdateStatus(message)));
+            else labelStatus.Text = message;
+        }
+
+        private void AddResult(string filePath, string sheet, string address, string text)
+        {
+            if (InvokeRequired) Invoke(new MethodInvoker(() => AddResult(filePath, sheet, address, text)));
+            else listResult.Items.Add(new ListViewItem(new[] {filePath, sheet, address, text}));
+        }
+
+        private void backgroundFinder_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundFinder_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            buttonSearch.Enabled = true;
+            textKeyword.Enabled = true;
+            labelStatus.Text = "Wait";
+
+            textKeyword.Focus();
+        }
+
+        private string[] FindFiles(string dir, string pattern)
+        {
+            try
+            {
+                return Directory.GetFiles(dir, pattern, SearchOption.AllDirectories);
+            }
+            catch
+            {
+                var files = new List<string>();
+                try
+                {
+                    files.AddRange(Directory.GetFiles(dir, pattern, SearchOption.TopDirectoryOnly));
+                }
+                catch
+                {
+                }
+                try
+                {
+                    foreach (var subDir in Directory.GetDirectories(dir))
+                    {
+                        files.AddRange(FindFiles(subDir, pattern));
+                    }
+                }
+                catch
+                {
+                }
+                return files.ToArray();
+            }
+        }
+
+        private void listResult_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                var items = listResult.SelectedItems[0].SubItems;
+                var excelPath = items[0].Text;
+                var sheetName = items[1].Text;
+                var address = items[2].Text;
+                Process.Start("excel.exe", excelPath + "#" + sheetName + "!" + address);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
